2cfc944efb33bcc5fc2693b64fe37f5db67e4ff2
Varaygue <38994398+Varaygue@users.noreply.github.com>
2024-02-03 23:38:49 +0100

    push fmod gitignore

--- Assets/Directory/Scripts/SC_MenuHandle.cs
+++ Assets/Directory/Scripts/SC_MenuHandle.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class SC_MenuHandle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject unitsMenu;
+    public fmod button_press;
+    public void OpenUnits()
     {
-        
-    }
+        unitsMenu.SetActive(true);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
